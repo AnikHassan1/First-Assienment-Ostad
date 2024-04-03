598633107000b8387192bf471e5f2e977780b09c
Anik Hassan1 <ah491606@gmail.com>
2024-04-03 23:54:50 +0600

    Condition add

--- calculator/Js/index.js
+++ calculator/Js/index.js
@@ -3,29 +3,38 @@ let numberTwo = document.getElementById("numberTwo");
 let btnOne = document.getElementById("btnOne");
 let option = document.getElementById("option");
 let text = document.getElementById("text");
+let textSms = document.getElementById("text-sms");
+
 btnOne.addEventListener("click", () => {
-  if((option.value = isNaN)) {
-    text.innerHTML = "Please Enter Valid Numbers";
+ 
+  if ((numberOne.value == '' || numberTwo.value =='' ||  isNaN(numberOne.value) ||isNaN(numberTwo.value) )) {
+    text.innerHTML = "Please Enter Valid Number";
+    textSms.classList.add("error");
     numberOne.value = "";
     numberTwo.value = "";
-  }else if(option.value == 1) {
+    
+  } else if (numberOne.value != '' && option.value == 1) {
     let sum = numberOne.value + numberTwo.value;
-    text.innerHTML = "Addition Number is" + " " + sum;
+    text.innerHTML = "Addition Result is" + " " + sum;
+    textSms.classList.add("success");
     numberOne.value = "";
     numberTwo.value = "";
   } else if (option.value == 2) {
     let Subtraction = numberOne.value - numberTwo.value;
-    text.innerHTML = "Subtraction Number is" + " " + Subtraction;
+    text.innerHTML = "Subtraction Result is" + " " + Subtraction;
+    textSms.classList.add("success");
     numberOne.value = "";
     numberTwo.value = "";
   } else if (option.value == 3) {
     let Multiplication = numberOne.value * numberTwo.value;
-    text.innerHTML = "Multiplication Number is" + " " + Multiplication;
+    text.innerHTML = "Multiplication Result is" + " " + Multiplication;
+    textSms.classList.add("success");
     numberOne.value = "";
     numberTwo.value = "";
   } else if (option.value == 4) {
     let Division = numberOne.value / numberTwo.value;
-    text.innerHTML = "Division Number is" + " " + Division;
+    text.innerHTML = "Division Result is" + " " + Division;
+    textSms.classList.add("success");
     numberOne.value = "";
     numberTwo.value = "";
   }
